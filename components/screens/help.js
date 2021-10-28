68161d4414208f09f04518adbe42bea98d580b91
see-simon <60693190+see-simon@users.noreply.github.com>
2021-10-28 13:33:08 +0200

    Create help.js

---
+++ components/screens/help.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+
+const help =()=>{
+
+    return(
+        <>
+            
+        </>
+    )
+
+}
+export default help
